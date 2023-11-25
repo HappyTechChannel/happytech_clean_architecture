c23d1c872f9b5f383cf09c48d94771e8bbe25dcc
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-11-25 19:30:24 +0200

    add user params

--- lib/features/user/domain/repositories/user_repository.dart
+++ lib/features/user/domain/repositories/user_repository.dart
@@ -1,7 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:happytech_clean_architecture/core/errors/failure.dart';
+import 'package:happytech_clean_architecture/core/params/params.dart';
 import 'package:happytech_clean_architecture/features/user/domain/entities/user_entitiy.dart';
 
 abstract class UserRepository {
-  Future<Either<Failure, UserEntity>> getUser();
-}
+  Future<Either<Failure, UserEntity>> getUser({required UserParams params});
+} 
--- lib/features/user/domain/usecases/get_user.dart
+++ lib/features/user/domain/usecases/get_user.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:happytech_clean_architecture/core/errors/failure.dart';
+import 'package:happytech_clean_architecture/core/params/params.dart';
 import 'package:happytech_clean_architecture/features/user/domain/entities/user_entitiy.dart';
 import 'package:happytech_clean_architecture/features/user/domain/repositories/user_repository.dart';
 
@@ -8,7 +9,7 @@ class GetUser {
 
   GetUser({required this.repository});
 
-  Future<Either<Failure, UserEntity>> call() {
-    return repository.getUser();
+  Future<Either<Failure, UserEntity>> call({required UserParams params}) {
+    return repository.getUser(params: params);
   }
 }
