212d41d46a89fedc1b1ae6c87285da91778eac0a
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-11-25 20:56:54 +0200

    user repo impl

---
+++ lib/features/user/data/repositories/user_repository_impl.dart
@@ -0,0 +1,39 @@
+import 'package:dartz/dartz.dart';
+import 'package:happytech_clean_architecture/core/connection/network_info.dart';
+import 'package:happytech_clean_architecture/core/errors/expentions.dart';
+import 'package:happytech_clean_architecture/core/errors/failure.dart';
+import 'package:happytech_clean_architecture/core/params/params.dart';
+import 'package:happytech_clean_architecture/features/user/data/datasources/user_local_data_source.dart';
+import 'package:happytech_clean_architecture/features/user/data/datasources/user_remote_data_source.dart';
+import 'package:happytech_clean_architecture/features/user/domain/entities/user_entitiy.dart';
+import 'package:happytech_clean_architecture/features/user/domain/repositories/user_repository.dart';
+
+class UserREpositoryImpl extends UserRepository {
+  final NetworkInfo networkInfo;
+  final UserRemoteDataSource remoteDataSource;
+  final UserLocalDataSource localDataSource;
+  UserREpositoryImpl(
+      {required this.remoteDataSource,
+      required this.localDataSource,
+      required this.networkInfo});
+  @override
+  Future<Either<Failure, UserEntity>> getUser(
+      {required UserParams params}) async {
+    if (await networkInfo.isConnected!) {
+      try {
+        final remoteUser = await remoteDataSource.getUser(params);
+        localDataSource.cacheUser(remoteUser);
+        return Right(remoteUser);
+      } on ServerException catch (e) {
+        return Left(Failure(errMessage: e.errorModel.errorMessage));
+      }
+    } else {
+      try {
+        final localUser = await localDataSource.getLastUser();
+        return Right(localUser);
+      } on CacheExeption catch (e) {
+        return Left(Failure(errMessage: e.errorMessage));
+      }
+    }
+  }
+}
