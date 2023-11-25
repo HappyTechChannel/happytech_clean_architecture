ddd67da426c017499932da17e77c33b7d7feeb04
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-11-25 19:26:51 +0200

    get user UseCase

---
+++ lib/features/user/domain/usecases/get_user.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:happytech_clean_architecture/core/errors/failure.dart';
+import 'package:happytech_clean_architecture/features/user/domain/entities/user_entitiy.dart';
+import 'package:happytech_clean_architecture/features/user/domain/repositories/user_repository.dart';
+
+class GetUser {
+  final UserRepository repository;
+
+  GetUser({required this.repository});
+
+  Future<Either<Failure, UserEntity>> call() {
+    return repository.getUser();
+  }
+}
